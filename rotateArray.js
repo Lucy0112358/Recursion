6064703399442c8c7976a793d4b6a9761b4adeb0
“lusinehsahakian@gmail.com“ <lusinehsahakian@gmail.com>
2022-02-21 16:50:34 +0400

    Fix: Added solution.

--- rotateArray.js
+++ rotateArray.js
@@ -1,8 +1,10 @@
-function rotateArray(array, N) {
-  let temp, i;
-  i = 0;
-while(i<N){
-    temp = 
+function rotateArray(array, N, i, temp) {
+  if (i === N) {
+    return array.concat(temp);
+  } else {
+    temp[i] = array[0];
+    array.shift();
+    return rotateArray(array, N, i + 1, temp);
+  }
 }
-  return array;
-}
+console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 4, 0, []));
